bddcdbc0d7ec0a060ace20cbacd792338073bd6e
Robin Gustafsson <robin@rgson.se>
2019-12-04 18:19:40 +0100

    Automate puzzle directory creation

---
+++ template.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# TODO: Solve the puzzle!
+
+def task1(input):
+    pass
+
+def task2(input):
+    pass
+
+input = open('input.txt', 'r').read()
+print(task1(input))
+print(task2(input))
