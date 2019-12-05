99e24916b7a75ffa2cd1abecfe9c31cfa5642045
Robin Gustafsson <robin@rgson.se>
2019-12-05 22:24:01 +0100

    Add Haskell template

---
+++ template.hs
@@ -0,0 +1,17 @@
+#!/usr/bin/runghc
+
+main :: IO ()
+main = do
+    input <- readFile "input.txt"
+    putStrLn $ task1 input
+    putStrLn $ task2 input
+
+task1 :: String -> String
+task1 input =
+    undefined
+
+task2 :: String -> String
+task2 input =
+    undefined
+
+-- TODO: Solve the puzzle!
